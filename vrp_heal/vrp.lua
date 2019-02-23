70f69bacd15632ed8e662963699634c68e11be9b
bradxy <36649225+bradxy@users.noreply.github.com>
2019-02-22 21:29:27 -0800

    fix resource name

--- vrp_heal/vrp.lua
+++ vrp_heal/vrp.lua
@@ -3,5 +3,5 @@ local Proxy = module("vrp", "lib/Proxy")
 local vRP = Proxy.getInterface("vRP")
 
 async(function()
-  vRP.loadScript("v_heal", "hospital_sv")
+  vRP.loadScript("vrp_heal", "hospital_sv")
 end)
