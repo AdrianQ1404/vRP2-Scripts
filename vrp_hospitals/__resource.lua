b43cb778a26ff61379085dadd517dfd1ce7b80ac
bradxy <36649225+bradxy@users.noreply.github.com>
2019-03-27 13:46:58 -0700

    added menu options
    made into a menu

--- vrp_hospitals/__resource.lua
+++ vrp_hospitals/__resource.lua
@@ -2,13 +2,12 @@ resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
 dependency "vrp"
 
-client_scripts {
-  'hospital_cl.lua'
-  
-}
-
 server_scripts {
   '@vrp/lib/utils.lua',
   'vrp.lua'
 
+}
+
+files{
+  "cfg/hospitals.lua"
 }
